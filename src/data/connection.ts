6ff2fd0ea6468e7c8effadba92fd9cea8abd92c6
anonymoussubmit56-coder <anonymoussubmit56@gmail.com>
2026-02-23 14:19:09 +0000

    changin to https

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -1,6 +1,6 @@
 import type { IConnection } from "../types";
 
 export const connection : IConnection = {
-    url: "http://ec2-18-132-248-91.eu-west-2.compute.amazonaws.com:8000/",
+    url: "https://rackle-kermit-hugeously.ngrok-free.dev/",
     header: { "Content-Type": "application/json" }
 }
