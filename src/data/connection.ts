7650f8b2b21828b2df4c6a23d07be7e27abe2f77
anonymoussubmit56-coder <anonymoussubmit56@gmail.com>
2026-03-08 23:01:12 +0000

    updating url

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -1,6 +1,6 @@
 import type { IConnection } from "../types";
 
 export const connection : IConnection = {
-    url: "http://localhost:8000/",
+    url: "https://94c3u3nhagfm9v-8000.proxy.runpod.net/",
     header: { "Content-Type": "application/json" }
 }
