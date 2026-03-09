a31d5b9dc6d325bc83c6f1833b741e140277ae82
anonymoussubmit56-coder <anonymoussubmit56@gmail.com>
2026-03-09 00:00:26 +0000

    chaning url

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -1,6 +1,6 @@
 import type { IConnection } from "../types";
 
 export const connection : IConnection = {
-    url: "https://94c3u3nhagfm9v-8000.proxy.runpod.net/",
+    url: "https://4tx2soyquq6klz-8000.proxy.runpod.net/",
     header: { "Content-Type": "application/json" }
 }
