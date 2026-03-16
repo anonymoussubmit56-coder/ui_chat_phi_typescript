4c6d249038973f248df7fb8dfca649bf29bb9e05
anonymoussubmit56-coder <anonymoussubmit56@gmail.com>
2026-03-16 16:42:03 +0100

    new url

--- src/data/connection.ts
+++ src/data/connection.ts
@@ -1,6 +1,6 @@
 import type { IConnection } from "../types";
 
 export const connection : IConnection = {
-    url: "https://zh2ifk246tammi-8000.proxy.runpod.net/",
+    url: "https://w5x2nvfkpztjh5-8000.proxy.runpod.net/",
     header: { "Content-Type": "application/json" }
 }
